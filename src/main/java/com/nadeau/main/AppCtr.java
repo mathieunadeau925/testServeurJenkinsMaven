9b5574d85547d45731b608c7626d08b79dad684e
1995046 <1995046@LI-S024-0T14>
2019-11-22 15:27:30 -0500

    second  push

--- src/main/java/com/nadeau/main/AppCtr.java
+++ src/main/java/com/nadeau/main/AppCtr.java
@@ -4,5 +4,6 @@ package com.nadeau.main;
 public class AppCtr {
     public static void main(String[] args) {
         System.out.println("Bonjour " + args[0] + ", " + args[1]);
+        System.out.println("Au revoir!");
     }
 }
